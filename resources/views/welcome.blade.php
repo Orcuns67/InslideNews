dae4c2c4e70b39210450f1a4d247c59036f785b3
orcunsarac <orcun.sarac@brightest.be>
2021-05-30 16:25:19 +0200

    1.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,44 @@
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('/css/inslide.css') }}" rel="stylesheet"> 
     
-
-    <title>InslideNews</title>
+    <link href="{{ asset('/css/inslide.css') }}" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <!-- Slideshow container -->
+    <h2>Automatic Slideshow</h2>
+
+
 <div class="slideshow-container">
 
-<!-- Full-width images with number and caption text -->
 <div class="mySlides fade">
   <div class="numbertext">1 / 3</div>
-  <img src="/images/img1.jpg" style="width:100%">
-  <div class="text">Caption Text</div>
+  <img src="/images/img3.jpg">
+  
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">2 / 3</div>
-  <img src="/images/img2.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
+  <img src="/images/img1.jpg">
+  
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 3</div>
-  <img src="/images/img3.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
+  <img src="/images/img2.jpg">
+  
 </div>
 
-<!-- Next and previous buttons -->
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
 
-<!-- The dots/circles -->
 <div style="text-align:center">
-<span class="dot" onclick="currentSlide(1)"></span>
-<span class="dot" onclick="currentSlide(2)"></span>
-<span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
 </div>
 </body>
-
 </html>
 <script src="/javascript/inslide.js"></script>
