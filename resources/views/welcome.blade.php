bd2ed6301a258d1e769a8580bae947e7a77529f4
orcunsarac <orcun.sarac@brightest.be>
2021-06-05 01:40:01 +0200

    frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,50 +10,352 @@
 </head>
 
 <body>
-
-  
-
-  
-
-  <div class="circle blue">InslideNews</div>
-
-  <div class='square-box'>
-    <div class='square-content'>
-      <h3>Russian War</h3>
+<div class="companybrand"><img class="companylogo" src="/images/nike.png" />
+<span class="companytitle">Company title</span>
+</div>
+<div class="content-width">
+  <div class="slideshow">
+    <!-- Slideshow Items -->
+    <div class="slideshow-items">
+      <div class="item">
+        <div class="item-image-container">
+          <img class="item-image" src="/images/img3.jpg" />
+        </div>
+        <!-- Staggered Header Elements -->
+        <div class="item-header">
+          <span class="vertical-part"><b>N</b></span>
+          <span class="vertical-part"><b>i</b></span>
+          <span class="vertical-part"><b>k</b></span>
+          <span class="vertical-part"><b>e</b></span>
+        </div>
+        <!-- Staggered Description Elements -->
+        <div class="item-description">
+          <span class="vertical-part">
+            <b>Title Article</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span> 
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span>
+          <span class="vertical-part">
+            <b>sit</b>
+          </span> 
+          <span class="vertical-part">
+            <b>amet,</b>
+          </span>
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span> 
+          <span class="vertical-part">
+            <b>adipiscing</b>
+          </span>
+          <span class="vertical-part">
+            <b>elit.</b>
+          </span> 
+          <span class="vertical-part">
+            <b>Pellentesque</b>
+          </span>
+          <span class="vertical-part">
+            <b>elementum</b>
+          </span> 
+          <span class="vertical-part">
+            <b>gravida</b>
+          </span>
+          <span class="vertical-part">
+            <b>ex</b>
+          </span> 
+          <span class="vertical-part">
+            <b>at</b>
+          </span>
+          <span class="vertical-part">
+            <b>maximus.</b>
+          </span> 
+          <span class="vertical-part">
+            <b>Nullam</b>
+          </span>
+          <span class="vertical-part">
+            <b>quis</b>
+          </span> 
+          <span class="vertical-part">
+            <b>leo</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum.</b>
+          </span> 
+          <span class="vertical-part">
+            <b>Lorem</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span> 
+          <span class="vertical-part">
+            <b>Nam</b>
+          </span> 
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span>
+          <span class="vertical-part">
+            <b>malesuada</b>
+          </span> 
+          <span class="vertical-part">
+            <b>blandit.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Mauris</b>
+          </span> 
+          <span class="vertical-part">
+            <b>vulputate</b>
+          </span>
+          <span class="vertical-part">
+            <b>purus</b>
+          </span> 
+          <span class="vertical-part">
+            <b>id</b>
+          </span>
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span> 
+          <span class="vertical-part">
+            <b>euismod</b>
+          </span>
+          <span class="vertical-part">
+            <b>varius.</b>
+          </span>      
+        </div>
+      </div>
+      <div class="item">
+        <div class="item-image-container">
+          <img class="item-image" src="/images/img1.jpg" />
+        </div>
+        <!-- Staggered Header Elements -->
+        <div class="item-header">
+          <span class="vertical-part"><b>S</b></span>
+          <span class="vertical-part"><b>p</b></span>
+          <span class="vertical-part"><b>o</b></span>
+          <span class="vertical-part"><b>t</b></span>
+          <span class="vertical-part"><b>i</b></span>
+          <span class="vertical-part"><b>f</b></span>
+          <span class="vertical-part"><b>y</b></span>
+        </div>
+        <!-- Staggered Description Elements -->
+        <div class="item-description">
+          <span class="vertical-part">
+            <b>Title Article</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span>
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span>
+          <span class="vertical-part">
+            <b>sit</b>
+          </span>
+          <span class="vertical-part">
+            <b>amet,</b>
+          </span>
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span>
+          <span class="vertical-part">
+            <b>adipiscing</b>
+          </span>
+          <span class="vertical-part">
+            <b>elit.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Pellentesque</b>
+          </span>
+          <span class="vertical-part">
+            <b>elementum</b>
+          </span>
+          <span class="vertical-part">
+            <b>gravida</b>
+          </span>
+          <span class="vertical-part">
+            <b>ex</b>
+          </span>
+          <span class="vertical-part">
+            <b>at</b>
+          </span>
+          <span class="vertical-part">
+            <b>maximus.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Nullam</b>
+          </span>
+          <span class="vertical-part">
+            <b>quis</b>
+          </span>
+          <span class="vertical-part">
+            <b>leo</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Lorem</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span>
+          <span class="vertical-part">
+            <b>Nam</b>
+          </span>
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span>
+          <span class="vertical-part">
+            <b>malesuada</b>
+          </span>
+          <span class="vertical-part">
+            <b>blandit.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Mauris</b>
+          </span>
+          <span class="vertical-part">
+            <b>vulputate</b>
+          </span>
+          <span class="vertical-part">
+            <b>purus</b>
+          </span>
+          <span class="vertical-part">
+            <b>id</b>
+          </span>
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span>
+          <span class="vertical-part">
+            <b>euismod</b>
+          </span>
+          <span class="vertical-part">
+            <b>varius.</b>
+          </span>
+        </div>
+      </div>
+      <div class="item">
+        <div class="item-image-container">
+          <img class="item-image" src="/images/img2.jpg" />
+        </div>
+        <!-- Staggered Header Elements -->
+        <div class="item-header">
+          <span class="vertical-part"><b>A</b></span>
+          <span class="vertical-part"><b>i</b></span>
+          <span class="vertical-part"><b>r</b></span>
+          <span class="vertical-part"><b>b</b></span>
+          <span class="vertical-part"><b>n</b></span>
+          <span class="vertical-part"><b>b</b></span>
+        </div>
+        <!-- Staggered Description Elements -->
+        <div class="item-description">
+          <span class="vertical-part">
+            <b>Title Article</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span>
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span>
+          <span class="vertical-part">
+            <b>sit</b>
+          </span>
+          <span class="vertical-part">
+            <b>amet,</b>
+          </span>
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span>
+          <span class="vertical-part">
+            <b>adipiscing</b>
+          </span>
+          <span class="vertical-part">
+            <b>elit.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Pellentesque</b>
+          </span>
+          <span class="vertical-part">
+            <b>elementum</b>
+          </span>
+          <span class="vertical-part">
+            <b>gravida</b>
+          </span>
+          <span class="vertical-part">
+            <b>ex</b>
+          </span>
+          <span class="vertical-part">
+            <b>at</b>
+          </span>
+          <span class="vertical-part">
+            <b>maximus.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Nullam</b>
+          </span>
+          <span class="vertical-part">
+            <b>quis</b>
+          </span>
+          <span class="vertical-part">
+            <b>leo</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Lorem</b>
+          </span>
+          <span class="vertical-part">
+            <b>ipsum</b>
+          </span>
+          <span class="vertical-part">
+            <b>Nam</b>
+          </span>
+          <span class="vertical-part">
+            <b>consectetur</b>
+          </span>
+          <span class="vertical-part">
+            <b>malesuada</b>
+          </span>
+          <span class="vertical-part">
+            <b>blandit.</b>
+          </span>
+          <span class="vertical-part">
+            <b>Mauris</b>
+          </span>
+          <span class="vertical-part">
+            <b>vulputate</b>
+          </span>
+          <span class="vertical-part">
+            <b>purus</b>
+          </span>
+          <span class="vertical-part">
+            <b>id</b>
+          </span>
+          <span class="vertical-part">
+            <b>dolor</b>
+          </span>
+          <span class="vertical-part">
+            <b>euismod</b>
+          </span>
+          <span class="vertical-part">
+            <b>varius.</b>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="controls">
+      <ul>
+        <li class="control" data-index="0"></li>
+        <li class="control" data-index="1"></li>
+        <li class="control" data-index="2"></li>
+      </ul>
     </div>
   </div>
-
-  <div id="para1"></div>
-
-
-  <div class="slideshow-container">
-
-    <div class="mySlides fade">
-      <div class="numbertext">1 / 3</div>
-      <img src="/images/img3.jpg">
-
-    </div>
-
-    <div class="mySlides fade">
-      <div class="numbertext">2 / 3</div>
-      <img src="/images/img1.jpg">
-
-    </div>
-
-    <div class="mySlides fade">
-      <div class="numbertext">3 / 3</div>
-      <img src="/images/img2.jpg">
-
-    </div>
-
-  </div>
-  <br>
-
-  <div style="text-align:center">
-    <span class="dot"></span>
-    <span class="dot"></span>
-    <span class="dot"></span>
-  </div>
+</div>
 </body>
 
 </html>
